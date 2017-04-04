edde3d6f11fcaa2dacd995e65c05f38a30c15ec4
Rafael Elías <rafama@Debian>
2017-04-04 08:04:54 -0600

    Inicia subida de archivos

---
+++ build/CMakeFiles/tv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tv.dir/main.cpp.o"
+  "tv.pdb"
+  "tv"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
